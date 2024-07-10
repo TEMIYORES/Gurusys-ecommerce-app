b1106c2f84f74783fea7b734ef75c3ad88e3db4d
Qayyum Ibrahim <temiyores1234@gmail.com>
2024-07-10 15:01:38 +0100

    Frontend: fetched all orders for admin dashboard orders component.

--- frontend/src/components/DashboardOrders.tsx
+++ frontend/src/components/DashboardOrders.tsx
@@ -1,7 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../services/api";
+import { OrderType } from "../utilities/Types";
 
 const DashboardOrders: React.FC = () => {
-  return <div>DashboardOrders</div>;
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    api
+      .get("/orders")
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching orders:", error);
+      });
+  }, []);
+
+  return (
+    <div className="flex-grow">
+      <div className="w-full bg-lightOrange rounded-md">
+        <div className="w-full p-2">
+          <div className="w-full grid grid-cols-4 gap-2 items-center text-xs font-bold text-headercolor">
+            <div>
+              <p>S/N</p>
+            </div>
+            <p>Reference</p>
+            <p>TotalAmount</p>
+            <p>Status</p>
+          </div>
+        </div>
+      </div>
+      {orders.length < 1 ? (
+        <div className="text-left text-sm font-semibold">No orders found.</div>
+      ) : null}
+      {orders.length
+        ? orders.map((order: OrderType, index: number) => {
+            return (
+              <div key={order.reference} className="w-full rounded-md">
+                <div className="w-full p-2">
+                  <div className="w-full grid grid-cols-4 gap-2 items-center text-xs font-bold text-headercolor">
+                    <div className="flex gap-2 text-sm font-normal">
+                      <p>{index + 1}.</p>
+                    </div>
+                    <p>{order.reference}</p>
+                    <p className="">{order.totalAmount}</p>
+                    <p
+                      className={`w-fit p-2 rounded-lg ${
+                        order.status === "Paid" ? "bg-green-300" : "bg-red-300"
+                      }`}
+                    >
+                      {order.status}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            );
+          })
+        : null}
+    </div>
+  );
 };
 
 export default DashboardOrders;
--- frontend/src/utilities/Types.ts
+++ frontend/src/utilities/Types.ts
@@ -2,7 +2,22 @@ export interface ProductType {
   _id: string;
   name: string;
   description: string;
-  image:string;
+  image: string;
   price: number;
   stock: number;
+}
+export interface OrderType {
+  reference: string;
+  products: [
+    {
+      _id: string;
+      name: string;
+      image: string;
+      price: number;
+      quantity: number;
+    }
+  ];
+  totalAmount: number;
+  customerInfo: Object;
+  status: string;
 }
